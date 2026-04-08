3266a06feca3e8591a951fc4eec5d0acfb5a0651
mohit <mohitgiri1103@gmail.com>
2026-04-09 05:02:17 +0530

    update lib/env.ts

--- lib/env.ts
+++ lib/env.ts
@@ -7,7 +7,8 @@ const envSchema = z.object({
   SMTP_USER: z.email("SMTP_USER must be a valid email"),
   SMTP_PASS: z.string().min(1, "SMTP_PASS is required"),
   // FROM_EMAIL: z.email("FROM_EMAIL must be a valid email"),
-  FROM_EMAIL: z.string().email().optional().default("noreply@unicore.com"),
+  // FROM_EMAIL: z.string().email().optional().default("noreply@unicore.com"),
+  FROM_EMAIL: z.string().min(1, "FROM_EMAIL is required"),
 
   // Google OAuth / GMail Configuration
   GOOGLE_CLIENT_ID: z.string().min(1, "GOOGLE_CLIENT_ID is required"),
